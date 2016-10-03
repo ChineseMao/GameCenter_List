dcb37d4ecf5dc89be6afe1cbf927b46ef25c27f5
毛韶谦 <shao-qian.mao@utsoft.cn>
2016-10-03 17:40:19 +0800

    设备判断

--- GameCenter_list/GameCenter_list/ViewController.m
+++ GameCenter_list/GameCenter_list/ViewController.m
@@ -17,13 +17,89 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //毛韶谦
+    //添加监听事件；
+    [self registerFoeAuthenticationNotification];
     
-    
-    
+    if ([self isGameCenterAvailable]) {
+        
+        [self authenticateLocalUser];
+    }
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+/**
+ *  GameCenter 的delegate 方法
+ *
+ *  @param  gameCenterViewController   GameCenter的控制器；
+ *
+ */
+- (void)gameCenterViewControllerDidFinish:(GKGameCenterViewController *)gameCenterViewController {
+    
+    
+}
+
+//身份验证
+- (void)authenticateLocalUser{
+    GKLocalPlayer *localPlayer = [GKLocalPlayer localPlayer];
+    
+    localPlayer.authenticateHandler = ^(UIViewController *viewController, NSError *error){
+        if (viewController != nil) {
+            [self presentViewController:viewController animated:YES completion:nil];
+        }
+        else{
+            if ([GKLocalPlayer localPlayer].authenticated) {
+                // Get the default leaderboard identifier.
+                
+                [[GKLocalPlayer localPlayer] loadDefaultLeaderboardIdentifierWithCompletionHandler:^(NSString *leaderboardIdentifier, NSError *error) {
+                    
+                    if (error != nil) {
+                        NSLog(@"%@", [error localizedDescription]);
+                    }
+                    else{
+                        
+                    }
+                }];
+            }
+            
+            else{
+                
+            }
+        }
+    };
+    
+}
+
+//用户变更检测
+- (void)registerFoeAuthenticationNotification{
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(authenticationChanged) name:GKPlayerAuthenticationDidChangeNotificationName object:nil];
+}
+
+- (void)authenticationChanged{
+    if([GKLocalPlayer localPlayer].isAuthenticated){
+        
+    }else{
+        
+    }
+}
+
+
+/**
+ *  判断设备是否支持GameCenter
+ *
+ *  @return YES 支持；
+ *
+ */
+- (BOOL) isGameCenterAvailable {
+    
+    Class GKClass = NSClassFromString(@"GKLocalPlayer");
+    
+    NSString *reqSysVer = @"4.1";
+    NSString *currSysVer = [[UIDevice currentDevice] systemVersion];
+    BOOL osVersionSupported = ([currSysVer compare:reqSysVer options:NSNumericSearch] != NSOrderedAscending);
+    NSLog(@"GKClass = %@, currSysVer = %@",GKClass,currSysVer);
+    return (GKClass && osVersionSupported);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
